587e1e1b977838ade0fb22e6e0c9feb89a1b63bb
klaudiawojciechowska <wojciechowskaklaudia444@gmail.com>
2025-04-09 18:53:36 +0200

    add two the same products

--- cypress/e2e/add-to-cart.cy.js
+++ cypress/e2e/add-to-cart.cy.js
@@ -2,7 +2,7 @@ describe("Add to Cart", () => {
     beforeEach(() => {
         cy.visit("/");
     });
-    it("should add one coffee to the cart and verify its details", () => {
+    it("should add Espresso to the cart and verify its details", () => {
         cy.get('[data-test="Espresso"]').click();
         cy.get('[data-test="checkout"]').contains('$10.00');
         cy.get(':nth-child(2) > a').contains('1').click()
@@ -13,5 +13,12 @@ describe("Add to Cart", () => {
         cy.get('.list-item > :nth-child(3)').contains("$10.00").should("be.visible");
 
     });
-
+it("should add two the same products", () => {
+    cy.get('[data-test="Espresso"]').click(); //add product to cart
+    cy.get('[data-test="Espresso"]').click(); //add product to cart
+    cy.get(':nth-child(2) > a').contains('2').click() //click on cart
+    cy.url().should("include", "/cart"); //go to cart page
+    cy.get('.list-header + .list-item').should('have.length', 1) //check if the cart has 1 product
+    cy.get(':nth-child(2) > .unit-desc').contains("x 2").should("be.visible"); //check if the price is correct
+})
 })
