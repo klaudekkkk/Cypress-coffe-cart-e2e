ae39694293b0908390f74f665d55e8d86d71fedd
klaudiawojciechowska <wojciechowskaklaudia444@gmail.com>
2025-04-09 19:26:03 +0200

    add all products to cart

--- cypress/e2e/add-to-cart.cy.js
+++ cypress/e2e/add-to-cart.cy.js
@@ -18,7 +18,33 @@ it("should add two the same products", () => {
     cy.get('[data-test="Espresso"]').click(); //add product to cart
     cy.get(':nth-child(2) > a').contains('2').click() //click on cart
     cy.url().should("include", "/cart"); //go to cart page
-    cy.get('.list-header + .list-item').should('have.length', 1) //check if the cart has 1 product
+    cy.get('li[data-v-8965af83=""]').should('have.length', 2) //check if the cart has 1 products firs li is header
     cy.get(':nth-child(2) > .unit-desc').contains("x 2").should("be.visible"); //check if the price is correct
 })
+    it("should add two different products", () => {
+        cy.get('[data-test="Espresso"]').click(); //add product to cart
+        cy.get('[data-test="Mocha"]').click(); //add product to cart
+        cy.get(':nth-child(2) > a').contains('2').click() //click on cart
+        cy.url().should("include", "/cart"); //go to cart page
+        cy.get('li[data-v-8965af83=""]').should('have.length', 3) //check if the cart has 2 products firs li is header
+    })
+    it.only("should add all products to cart", () => {
+            const coffeeTypes = [
+                "Espresso",
+                "Espresso_Macchiato",
+                "Cappuccino",
+                "Mocha",
+                "Flat_White",
+                "Americano",
+                "Cafe_Latte",
+                "Espresso_Con Panna",
+                "Cafe_Breve"
+            ];
+        coffeeTypes.forEach((coffeeType) => {
+            cy.get(`[data-test="${coffeeType}"]`).click(); //add all products to cart
+        });
+            cy.get(':nth-child(2) > a').contains('9').click() //click on cart
+            cy.url().should("include", "/cart"); //go to cart page
+            cy.get('li[data-v-8965af83=""]').should('have.length', 10) //check if the cart has 2 products firs li is header
+        })
 })
